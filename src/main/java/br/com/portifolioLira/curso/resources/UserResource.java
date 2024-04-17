55785d507751836c8ad58af74691425f7071a227
ProjetosEltonLira <eltondelira@gmail.com>
2024-04-16 23:10:24 -0300

    User delete

--- src/main/java/br/com/portifolioLira/curso/resources/UserResource.java
+++ src/main/java/br/com/portifolioLira/curso/resources/UserResource.java
@@ -16,12 +16,12 @@ import java.util.List;
 public class UserResource {
 
     @Autowired
-    private UserService userService;
+    private UserService service;
 
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
 
-        List<User> list = userService.findAll();
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
         //Versão mokada do retorno
         // User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
@@ -30,14 +30,14 @@ public class UserResource {
 
     @GetMapping (value = "/{id}") // Isso indica que permite receber um valor na URL
     public ResponseEntity<User> findById(@PathVariable Long id) { // para o Spring aceitar esse valor vindo da URL precisa colocar a notação @PathVariable
-        User user = userService.findById(id);
+        User user = service.findById(id);
         return ResponseEntity.ok().body(user);
     }
 
     //usado para fazer o Insert, tem que usar o POST no INSOMNIA
     @PostMapping
     public ResponseEntity<User> insert(@RequestBody User user){
-        userService.insert(user);
+        service.insert(user);
         URI uri = ServletUriComponentsBuilder
                 .fromCurrentRequest()
                 .path("/{id}")
@@ -45,4 +45,11 @@ public class UserResource {
                 .toUri();
         return ResponseEntity.created(uri).body(user); //ResposeEntity.create
     }
+
+    @DeleteMapping (value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/br/com/portifolioLira/curso/services/UserService.java
+++ src/main/java/br/com/portifolioLira/curso/services/UserService.java
@@ -28,5 +28,9 @@ public class UserService {
         return repository.save(user);
     }
 
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
+
 
 }
